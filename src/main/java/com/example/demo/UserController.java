959e4dce05b998b34e90671e2c8bfe7cbbcddb7c
vhuy16 <109493886+vhuy16@users.noreply.github.com>
2024-01-17 23:12:39 +0700

    test

---
+++ src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+    @GetMapping()
+    public String getUser(){
+        return "hello";
+    }
+}
